aef88c893d317d625c5fc88a2ad12c622261c59a
HamzaKotob <hamza.kotob@outlook.com>
2023-12-14 21:10:34 +0200

    first commit

---
+++ src/components/sections/Features.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import Data from "@data/sections/features.json";
+import Accordion from 'react-bootstrap/Accordion';
+import ModalVideo from 'react-modal-video';
+import 'react-modal-video/css/modal-video.css';
+
+const FeaturesSection = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <section className="core-features">
+        <div className="container">
+        <div className="row">
+            <div className="col-lg-6" >
+            <div className="space">
+                <div className="heading-style-2">
+                <div className="container">
+                    <div className="row">
+                    <div className="col-lg-12">
+                        <div className="data">
+                            <span>{Data.subtitle}</span>
+                            <h2>{Data.title}</h2>
+                        </div>
+                    </div>         
+                    </div>
+                </div>
+                </div>
+
+                <Accordion defaultActiveKey="features-acc-0">
+                    {Data.items.map((item, key) => (
+                    <Accordion.Item key={`features-item-${key}`} eventKey={`features-acc-${key}`}>
+                        <Accordion.Header>
+                            <span className="num">{item.num}</span> {item.title}
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            <p>{item.text}</p>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    ))}
+                </Accordion>
+
+            </div>
+            </div>
+            <div className="col-lg-6" >
+                <div className="shape">
+                    <div className="video">
+                        <figure>
+                            <img src={Data.image.url} alt={Data.image.alt} />
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+
+        <ModalVideo channel='youtube' isOpen={isOpen} videoId={Data.yt_video_id} onClose={() => setOpen(false)} />
+    </section>
+  );
+};
+
+export default FeaturesSection;
---
+++ src/data/projects/project-06.md
@@ -0,0 +1,33 @@
+---
+#preview
+title: AL HAYAT HOSPITAL
+image: /img/lau.jpg
+short: "Solar energy system.(39 Nos. solar collectors,
+2 Nos. Hot Water Storage
+Tanks Capacity=1500L each)"
+location: ""
+dates: "2014"
+
+
+details:
+    items:
+        - label: Mechanical Contractor
+          value: MSTech For Engineering S.A.R.L
+
+        - label: Completion Date
+          value: 2014
+        
+
+#full details
+checklist:
+    title: Scope Of Work
+    items:
+        - Solar energy system.(39 Nos. solar collectors,
+        - 2 Nos. Hot Water Storage Tanks Capacity=1500L each)
+
+
+slider: 
+    items:
+        - image: /img/lau.jpg
+          alt: "image"
+---
