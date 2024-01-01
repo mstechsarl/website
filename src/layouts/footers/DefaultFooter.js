cefd1e1a6c5ff632a8c3fa04ae98f6c18a756dd1
mstechsarl <152030432+mstechsarl@users.noreply.github.com>
2024-01-01 13:24:44 +0200

    Update DefaultFooter.js

--- src/layouts/footers/DefaultFooter.js
+++ src/layouts/footers/DefaultFooter.js
@@ -132,9 +132,9 @@ const DefaultFooter = ({ en }) => {
                 </ul>
               </div>
             </div>
-            <div className="col-lg-4 col-md-6 col-sm-12">
+             <div className="col-lg-4 col-md-6 col-sm-12">
               <div className="footer-col">
-                <h3>Contact In Lebanon</h3>
+                <h3>Contact In Germany</h3>
                 <ul>
                   <li>
                     <svg
@@ -172,7 +172,7 @@ const DefaultFooter = ({ en }) => {
                         />{" "}
                       </g>{" "}
                     </svg>
-                    <p>{data.footer.info2.address}</p>
+                    <p>{data.footer.info.address}</p>
                   </li>
                   <li>
                     <svg
@@ -206,7 +206,7 @@ const DefaultFooter = ({ en }) => {
                         />{" "}
                       </g>{" "}
                     </svg>
-                    <p>{data.footer.info2.tel}</p>
+                    <p>{data.footer.info.tel}</p>
                   </li>
                   <li>
                     <svg
@@ -244,7 +244,7 @@ const DefaultFooter = ({ en }) => {
                         />{" "}
                       </g>{" "}
                     </svg>
-                    <p>{data.footer.info2.email}</p>
+                    <p>{data.footer.info.email}</p>
                   </li>
                 </ul>
               </div>
