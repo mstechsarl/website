28a089487b962476779cecfb32f18b207d758bac
youssef sk <alsarakibiy@gmail.com>
2024-01-05 17:28:44 +0300

    fix photo naming

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,11 +11,20 @@ class MyDocument extends Document {
           {/* meta end */}
 
           {/* favicon begin */}
-          <link rel="apple-touch-icon" sizes="76x76" href="/favicon/ms-icon-150x150" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/ms-icon-150x150" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/ms-icon-150x150" />
+          {/* <link rel="apple-touch-icon" sizes="76x76" href="/favicon/ms-icon-150x150" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/ms-icon-150x150" /> */}
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/img/logo_ill.png"
+          />
           <link rel="manifest" href="/favicon/manifest.json" />
-          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
           {/* favicon */}
