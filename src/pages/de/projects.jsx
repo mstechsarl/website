35f3f5d23ec17420f0eaaf0894bbaef9c167c005
mstechsarl <152030432+mstechsarl@users.noreply.github.com>
2024-01-08 12:17:36 +0200

    Update projects.jsx

--- src/pages/de/projects.jsx
+++ src/pages/de/projects.jsx
@@ -1,80 +1,56 @@
 import Layouts from "@layouts/Layouts";
 import Link from "next/link";
-
-import { getSortedProjectsData } from "@library/projects";
 import CallToActionSection from "@components/sections/CallToAction";
+import { getSortedProjectsData } from "@library/projects";
+
+
 import PageBanner from "@components/PageBanner";
 
 const Portfolio = (props) => {
   return (
-    <Layouts>
-      <PageBanner pageTitle={"Projekte im Libanon"} pageDesc={""} where={"l"} />
+    <Layouts en>
+      <PageBanner pageTitle={"Projects in Lebanon"} pageDesc={""} where="l" en />
 
-      {/* Our Project One Start */}
-      <section className="gap no-top project-completed our-projects-one">
-        {props.projects.map((item, key) => (
-          <div key={`projects-item-${key}`} className="prj-post">
-            <div className="container">
-              <div className="row align-items-center">
-                <div className="col-lg-6">
-                  <div className="proj-data">
+      {/* Our Project Two Start */}
+      <section className="gap project-style-one addition">
+        <div className="container">
+          <div className="row project-slider">
+            {props.projects.map((item, key) => (
+              <div key={`projects-item-${key}`} className="col-lg-6">
+                <div className="project-post">
+                  <figure>
+                    <img className="w-100" src={item.image} alt={item.title} />
+                  </figure>
+                  <div className="project-data">
                     <h3>
-                      <Link href={`/de/projects/${item.id}`}>{item.title}</Link>
+                      <Link href={`/en/projects/${item.id}`}>
+                        {item.title}
+                      </Link>
                     </h3>
                     <p>{item.short}</p>
-                    <div className="loc-date">
-                      <div>
-                        <span>LOCATION:</span>
-                        <span>{item.location}</span>
-                      </div>
-                      <div>
-                        <span>DATE:</span>
-                        <span>{item.dates}</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-6">
-                  <div className="data">
-                    <figure>
-                      <img src={item.image} alt={item.title} />
-                    </figure>
+                    <Link
+                      className="project-icon"
+                      href={`/en/projects/${item.id}`}
+                    >
+                      <i className="fa-solid fa-angles-right" />
+                    </Link>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-        ))}
-        {/*
-        <div className="container">
-          <div className="row">
-            <div className="builty-pagination">
-              <nav aria-label="Page navigation example">
-                <ul className="pagination">
-                  <li className="page-item"><a className="page-link" href="#."><i className='fa-solid fa-arrow-left-long'></i></a></li>
-                  <li className="page-item"><a className="page-link" href="#.">01</a></li>
-                  <li className="page-item"><a className="page-link" href="#.">02</a></li>
-                  <li className="page-item"><a className="page-link" href="#.">03</a></li>
-                  <li className="page-item space"><a className="page-link" href="#.">..........</a></li>
-                  <li className="page-item"><a className="page-link" href="#.">08</a></li>
-                  <li className="page-item"><a className="page-link" href="#."><i className='fa-solid fa-arrow-right-long'></i> </a></li>
-                </ul>
-              </nav>
-            </div>
+            ))}
           </div>
         </div>
-        </section>
-        */}
-        {/* Our Project One End */}
       </section>
-      <CallToActionSection />
+      <CallToActionSection en />
+
+      {/* Our Project Two End */}
     </Layouts>
   );
 };
 export default Portfolio;
 
 export async function getStaticProps() {
-  const allProjects = getSortedProjectsData();
+  const allProjects = getSortedProjectsData(true);
 
   return {
     props: {
