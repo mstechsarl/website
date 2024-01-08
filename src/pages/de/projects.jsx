a0200f9508a4da2e5f5462ea5c1ee5deb44e477a
mstechsarl <152030432+mstechsarl@users.noreply.github.com>
2024-01-08 12:45:18 +0200

    Update projects.jsx

--- src/pages/de/projects.jsx
+++ src/pages/de/projects.jsx
@@ -9,7 +9,7 @@ import PageBanner from "@components/PageBanner";
 const Portfolio = (props) => {
   return (
     <Layouts de>
-      <PageBanner pageTitle={"Projects in Lebanon"} pageDesc={""} where="l" en />
+      <PageBanner pageTitle={"Projects in Lebanon"} pageDesc={""} where="l" de />
 
       {/* Our Project Two Start */}
       <section className="gap project-style-one addition">
