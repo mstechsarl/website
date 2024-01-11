de7238431a31cffa4814dd1e7d48f473a4d52e0c
mstechsarl <152030432+mstechsarl@users.noreply.github.com>
2024-01-11 17:26:33 +0200

    Create RelatedProjectsDe2.jsx

---
+++ src/components/RelatedProjectsDe2.jsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+const RelatedProjects = ({ projects, en }) => {
+  return (
+    <>
+      <section className="gap no-top project-style-one extra addition">
+        <div className="heading">
+          <figure>
+            <img src="/images/heading-icon.png" alt="Heading Icon" />
+          </figure>
+          <span>Firmenprojekte</span>
+          <h2>Verwandte Projekte</h2>
+        </div>
+        <div className="container">
+          <div className="row project-slider">
+            {projects.slice(0, 2).map((item, key) => (
+              <div key={`relatedprojects-item-${key}`} className="col-lg-6">
+                <div className="project-post">
+                  <figure>
+                    <img src={item.image} alt={item.title} />
+                  </figure>
+                  <div className="project-data">
+                    <h3>
+                      <Link href={`/${en ? "en" : "de"}/projects2/${item.id}`}>
+                        {item.title}
+                      </Link>
+                    </h3>
+                    <p>{item.short}</p>
+                    <Link
+                      className="project-icon"
+                      href={`/${en ? "en" : "de"}/projects2/${item.id}`}
+                    >
+                      <i className="fa-solid fa-angles-right" />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default RelatedProjects;
