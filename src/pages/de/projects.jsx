869fa09bbfb431f8dde5cf524ef4eb6ce54be181
youssef sk <alsarakibiy@gmail.com>
2024-01-12 20:12:20 +0300

    fixed projects in de

--- src/pages/de/projects.jsx
+++ src/pages/de/projects.jsx
@@ -3,13 +3,17 @@ import Link from "next/link";
 import CallToActionSection from "@components/sections/CallToAction";
 import { getSortedProjectsData } from "@library/projects";
 
-
 import PageBanner from "@components/PageBanner";
 
 const Portfolio = (props) => {
   return (
     <Layouts de>
-      <PageBanner pageTitle={"Projeckte im libanon"} pageDesc={""} where="l" de />
+      <PageBanner
+        pageTitle={"Projeckte im libanon"}
+        pageDesc={""}
+        where="l"
+        de
+      />
 
       {/* Our Project Two Start */}
       <section className="gap project-style-one addition">
@@ -23,9 +27,7 @@ const Portfolio = (props) => {
                   </figure>
                   <div className="project-data">
                     <h3>
-                      <Link href={`/de/projects/${item.id}`}>
-                        {item.title}
-                      </Link>
+                      <Link href={`/de/projects/${item.id}`}>{item.title}</Link>
                     </h3>
                     <p>{item.short}</p>
                     <Link
@@ -50,7 +52,7 @@ const Portfolio = (props) => {
 export default Portfolio;
 
 export async function getStaticProps() {
-  const allProjects = getSortedProjectsData(true);
+  const allProjects = getSortedProjectsData(false);
 
   return {
     props: {
